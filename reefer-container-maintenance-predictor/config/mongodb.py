c1eff3b94db04f9c981ae847d2a1d98a9be75f70
estherh <estherh@il.ibmcom>
2019-08-21 14:59:46 +0300

    mongodb setting

--- reefer-container-maintenance-predictor/config/mongodb.py
+++ reefer-container-maintenance-predictor/config/mongodb.py
@@ -15,16 +15,16 @@ def mongo_config():
         return
 
     user = os.getenv('MONGODB_USER', '')
-    # if not user:
-    #     sys.exit('missing MONGODB_USER env var')
-    #     return
+    if not user:
+        sys.exit('missing MONGODB_USER env var')
+        return
 
     password = os.getenv('MONGODB_PASSWORD', '')
-    # if not password:
-    #     sys.exit('missing MONGODB_PASSWORD env var')
-    #     return
+    if not password:
+        sys.exit('missing MONGODB_PASSWORD env var')
+        return
 
-    database = os.getenv('MONGODB_DATABASE', 'admin')
+    database = os.getenv('MONGODB_DATABASE', '')
     if not database:
         sys.exit('missing MONGODB_DATABASE env var')
         return
